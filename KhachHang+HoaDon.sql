5792933bcf76888fda1b15f20d7d9dc4315f0c11
lantern <137916111+HIepNgoc2006@users.noreply.github.com>
2025-10-09 13:48:59 +0700

    Update KhachHang+HoaDon.sql

--- KhachHang+HoaDon.sql
+++ KhachHang+HoaDon.sql
@@ -1,47 +1,51 @@
+CREATE DATABASE ban_hang_db;
+GO
+
+USE ban_hang_db;
+GO
+
 CREATE TABLE KhachHang (
     IdKhachHang INT IDENTITY(1,1) NOT NULL,
     TenKhachHang NVARCHAR(100) NOT NULL,
-    MaKhachHang VARCHAR(50) NOT NULL,
-    Email NVARCHAR (250) NULL,
+	Email NVARCHAR (250) NULL,
     DiaChi NVARCHAR(250) NULL,
     SDT VARCHAR(20) NULL,
     NgayTao DATETIME2 DEFAULT GETDATE(),
 
     CONSTRAINT PK_KhachHang PRIMARY KEY CLUSTERED (IdKhachHang),
-    CONSTRAINT UQ_KhachHang_MaKhachHang UNIQUE (MaKhachHang),
-    CONSTRAINT UQ_KhachHang_SDT UNIQUE (SDT)
+    CONSTRAINT UQ_KhachHang_SDT UNIQUE (SDT),
 );
 GO
+
+
 
 CREATE TABLE DonDatHang (
     IdDonHang INT IDENTITY(1,1) NOT NULL,
     IdKhachHang INT NOT NULL,
-    SoDonHang VARCHAR(50) NOT NULL,
     NgayDatHang DATETIME2 DEFAULT GETDATE(),
-    LoaiDonHang NVARCHAR(20) NULL,
-    TongTien DECIMAL(18,0) DEFAULT 0,
-    TrangThai INT NOT NULL DEFAULT 1, 
-    
+    LoaiDonHang INT NOT NULL DEFAULT 1, -- 1: Online, 2: Offline
+    TongTien DECIMAL(18,0) DEFAULT 0, 
+    TrangThai INT NOT NULL DEFAULT 1,  -- 1: Mới, 2: Đã xác nhận, 3: Hoàn thành, 4: Hủy
+	
+	
+
     CONSTRAINT PK_DonDatHang PRIMARY KEY CLUSTERED (IdDonHang),
-    CONSTRAINT UQ_DonDatHang_SoDonHang UNIQUE (SoDonHang),
-    CONSTRAINT FK_DonDatHang_KhachHang FOREIGN KEY (IdKhachHang) REFERENCES KhachHang(IdKhachHang) 
+    CONSTRAINT FK_DonDatHang_KhachHang FOREIGN KEY (IdKhachHang) REFERENCES KhachHang(IdKhachHang)
 );
 GO
 
 CREATE TABLE HoaDon (
     IDHoaDon INT IDENTITY(1,1) NOT NULL,
-    IdDonHang INT NOT NULL,
-    MaMatHang VARCHAR(50) NOT NULL, 
+	IdDonHang INT NOT NULL,
     NgayTao DATETIME2 DEFAULT GETDATE(),
-    NgayThanhToan DATETIME2 NULL,
-    
-    SoLuong INT NOT NULL,
-    GiaBan DECIMAL(18,0) NOT NULL,
-    TongTien AS (GiaBan * SoLuong), 
+	NgayThanhToan DATETIME2 NUll,
+	SoLuong INT NOT NULL DEFAULT 1,
+    GiaBan DECIMAL(30,0) NOT NULL,
+    TongTien AS (GiaBan * SoLuong),
 
-    CONSTRAINT PK_HoaDon PRIMARY KEY CLUSTERED (IDHoaDon), 
-    CONSTRAINT FK_HD_DonDatHang FOREIGN KEY (IdDonHang) REFERENCES DonDatHang(IdDonHang), 
-    CONSTRAINT CK_HD_SoLuong CHECK (SoLuong > 0),
-    CONSTRAINT CK_HD_GiaBan CHECK (GiaBan >= 0)
+    CONSTRAINT PK_HoaDon PRIMARY KEY CLUSTERED (IDHoaDon),
+    CONSTRAINT FK_HoaDon_DonDatHang FOREIGN KEY (IdDonHang) REFERENCES DonDatHang(IdDonHang),
+    CONSTRAINT CK_HoaDon_SoLuong CHECK (SoLuong > 0),
+    CONSTRAINT CK_HoaDon_GiaBan CHECK (GiaBan >= 0)
 );
 GO
